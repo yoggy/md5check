7e6e17a8f8b19208dfe299808f1e8d36d43eb33c
yoggy <yoggy0@gmail.com>
2013-07-28 19:20:58 +0900

    modified:   main_md5create.cpp

--- main_md5create.cpp
+++ main_md5create.cpp
@@ -7,15 +7,20 @@ int main(int argc, char *argv[])
 
 	std::string db_file = argv[1];
 
+	// target path
 	std::vector<std::string> paths;
 	for (int i = 2; i < argc; ++i) {
 		paths.push_back(argv[i]);
 	}
+	if (exists(paths) == false) {
+		return 1;
+	}
 
+	// calc md5hash
 	std::map<std::string, std::string> md5map;
-
 	process(paths, md5map);
 
+	// save 
 	save_md5map(db_file, md5map);
 	
 	return 0;
